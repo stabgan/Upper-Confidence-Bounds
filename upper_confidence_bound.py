041e1f60aeb05e8a923b32868781274f26bdbf7b
Kaustabh Ganguly <stabgan@users.noreply.github.com>
2026-03-14 16:18:56 +0530

    Reapply "fix: modernize code + rewrite README"
    This reverts commit 4e4ff52a0dbd9960807ac89ed095a9cebdac2786.

--- upper_confidence_bound.py
+++ upper_confidence_bound.py
@@ -1,6 +1,8 @@
-# Upper Confidence Bound
+# Upper Confidence Bound (UCB) Algorithm
+# Solves the multi-armed bandit problem for ad CTR optimization
 
-# Importing the libraries
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,7 +11,6 @@ import pandas as pd
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 # Implementing UCB
-import math
 N = 10000
 d = 10
 ads_selected = []
@@ -20,23 +21,24 @@ for n in range(0, N):
     ad = 0
     max_upper_bound = 0
     for i in range(0, d):
-        if (numbers_of_selections[i] > 0):
+        if numbers_of_selections[i] > 0:
             average_reward = sums_of_rewards[i] / numbers_of_selections[i]
-            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
+            delta_i = math.sqrt(3 / 2 * math.log(n + 1) / numbers_of_selections[i])
             upper_bound = average_reward + delta_i
         else:
-            upper_bound = 1e400
+            upper_bound = math.inf
         if upper_bound > max_upper_bound:
             max_upper_bound = upper_bound
             ad = i
     ads_selected.append(ad)
-    numbers_of_selections[ad] = numbers_of_selections[ad] + 1
+    numbers_of_selections[ad] += 1
     reward = dataset.values[n, ad]
-    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
-    total_reward = total_reward + reward
+    sums_of_rewards[ad] += reward
+    total_reward += reward
 
 # Visualising the results
-plt.hist(ads_selected)
+plt.hist(ads_selected, bins=range(d + 1), edgecolor='black', align='left')
+plt.xticks(range(d))
 plt.title('Histogram of ads selections')
 plt.xlabel('Ads')
 plt.ylabel('Number of times each ad was selected')
